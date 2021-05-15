440d603780b5293b6f3b3cd1080730ce6e6bc663
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-05-16 00:23:18 +0530

    15/05/2021 night

--- hardwork/test.py
+++ hardwork/test.py
@@ -1,3 +1,14 @@
-n = int(input())
-ans = "codeforces"
-print(ans+"s"*(n-1))
+n,m = map(str,input().split())
+m = int(m)
+for i in range(m):
+    n = list(str(n))
+    k = n.copy()
+    n.sort()
+    k.sort(reverse=True)
+    n = int("".join(n))
+    k = int("".join(k))
+    n = abs(n-k)
+    if n==0:
+        break 
+print(n)    
+    
