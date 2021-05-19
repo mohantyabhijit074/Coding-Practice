5a5e82a5698879f005d2bcf378cc9f8c795d809a
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-05-19 11:35:48 +0530

    19/05/2021

--- hardwork/test.cpp
+++ hardwork/test.cpp
@@ -6,46 +6,18 @@ using namespace std;
 
 void solve()
 {
-	int n,m;
-	cin >> n >> m;
-	vector<int> arr(n);
-	vector<int> brr(m);
-	sort(arr.begin(),arr.end());
-	for(int i=0;i<n;i++)
-	{
-		cin >> arr[i];
-	}
-	for(int j=0;j<m;j++)
-	{
-		cin >> brr[j];
-	}
-	vector<int> res(m,0);
-	int j = 0;
-	for(int i=0;i<m;i++)
-	{
-		while(j<n && arr[j]<brr[i])
-		{
-			j++;
-		}
-		res[i] = j;
-	}
-	for(auto i : res)
-	{
-		cout << i <<" ";
-	}
-	cout <<"\n";
-	return;
+
 }
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	ll t;
-	//cin >> t;
-	t = 1;
-	while(t--)
-	{
-		solve();
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    //cin >> t;
+    t = 1;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
 }
--- hardwork/test.py
+++ hardwork/test.py
@@ -1,8 +1,29 @@
-n = int(input())
-li = []
-for i in range(n):
-    a,b = map(str,input().split())
-    b = int(b)
-    li.append([a,b])
-li.sort(key = lambda x:x[1])
-print(li[-2][0])
+import math
+t = int(input())
+for _ in range(t):
+    n,w = map(int,input().split())
+    li = list(map(int,input().split()))
+    k = li.copy()
+    k.sort()
+    if k[0]>w:
+        print(-1)
+    elif sum(li)<w/2:
+        print(-1)
+    else:
+        temp = []
+        f = 0
+        for i in range(n):
+            temp.append([i,li[i]])
+        #print(temp)
+        ans = []
+        temp.sort(key = lambda x:x[1],reverse = True)
+        s = 0
+        for i in range(n):
+            if temp[i][1]+s<=w:
+                s+=temp[i][1]
+                ans.append(temp[i][0]+1)
+        if s<math.ceil(w/2):
+            print(-1)
+        else:
+            print(len(ans))
+            print(*ans)
