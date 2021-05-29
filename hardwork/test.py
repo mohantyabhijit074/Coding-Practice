58861e729763b060bab7c7d04d573b065d5abe2e
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-05-29 23:52:35 +0530

    28/05/2021

--- hardwork/test.py
+++ hardwork/test.py
@@ -1,16 +1,35 @@
-li = list(map(int,input().split()))
-res = [0]*len(li)
-li = [abs(i) for i in li]
-i = 0
-j = len(li)-1
-k = len(li)-1
-while i<=j:
-    if li[i]<li[j]:
-        res[k] = li[j]**2
-        k-=1
-        j-=1
-    else:
-        res[k] = li[i]**2
-        k-=1
-        i+=1
-print(*res)
+from itertools import permutations 
+n = list(map(int,input().split()))
+m = list(map(int,input().split()))
+a = []
+b = []
+for i in range(len(n)):
+    for j in range(len(m)):
+        if n[i]==m[j]:
+            a.append(n[i])
+            b.append(m[j])
+for i in a:
+    n.remove(i)
+for i in b:
+    m.remove(i)
+temp = []
+ans = 0
+n.sort()
+m.sort()
+p = 0
+k = 0
+for i in range(len(n)):
+    res = pow(10,10)
+    for j in range(len(m)):
+        if n[i]^m[j] < res and j not in temp:
+            res = n[i]^m[j]
+            p = j 
+    print(n[i],m[p])
+    temp.append(p)
+    ans+=res 
+print(ans)
+            
+
+
+
+    
