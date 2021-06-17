6306981160f44ab7beef1c2b61fe9e45c4c6b030
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-06-18 00:59:26 +0530

    17/06/2021

--- hardwork/test.py
+++ hardwork/test.py
@@ -1,32 +1,52 @@
+import math
+sieve = []
+pre = []
+cnt = 0
+def sieve_of_erost(n):
+    global sieve
+    sieve=[1]*(n+1) 
+    pre = [1]*(n+1)
+    i = 1
+    sieve[0]=0
+    for i in range(2,math.ceil(math.sqrt(n))):
+        if sieve[i]==1:
+            for j in range(i*i,n+1,i):
+                if j%i==0:
+                    sieve[j] = 0
+    global cnt
+    for i in range(2,n+1):
+        if(sieve[i]==0):
+            pre[cnt] = i 
+            cnt+=1 
+    #print(sieve)
+
 def solve():
-    n,k = map(int,input().split())
+    n = int(input())
     li = list(map(int,input().split()))
+    i = 0
+    j = 0
     d = {}
-    for i in li:
-        if i not in d:
-            d[i] = 1 
+    ans = -1
+    while i<n and j<n:
+        if li[j] not in d:
+            d[li[j]] = 1 
         else:
-            d[i] +=1 
-    for i in d:
-        if k==0:
-            break 
-        k-=d[i]-1 
-        d[i] = 1
-    if k>0:
-        for i in d:
-            if k==0:
-                break 
-            k-=1 
-            d[i] = 0 
-    if k>0:
-        print(0)
-        return 
-    c = 0
-    for i in d:
-        if d[i]>0:
-            c+=1
-    print(c)
-    return 
+            d[li[j]] +=1
+        if d[li[j]]==1 and sieve[li[j]]==1:
+            j+=1 
+        else:
+            ans = max(ans,j-i)
+            i=j
+            j=i+1
+    ans = max(ans,j-i)
+    # for i in range(n-1):
+    #     if li[i]%li[i+1]!=0 or li[i+1]%li[i]!=0:
+    #         ans = 2
+    #         print("HERE")
+    if ans == 1:
+        ans = -1
+    print(ans)
 t = int(input())
+sieve_of_erost(pow(10,6)+1)
 for _ in range(t):
     solve()
