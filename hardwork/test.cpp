3706da1349b8fcf1da3d82703e08823fefc76a0b
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-06-19 10:14:21 +0530

    18/06/2021

--- hardwork/test.cpp
+++ hardwork/test.cpp
@@ -3,75 +3,25 @@ using namespace std;
 #include<bits/stdc++.h>
 #define mod 1000000007
 #define ll long long
-
-void bfs(vector<vector<ll>>&adj,vector<ll>&parent,vector<ll>&vis)
-{
-    vis[1] = 1;
-    queue<ll> q;
-    q.push(1);
-    while(!q.empty())
-    {
-        ll temp = q.front();
-        q.pop();
-        for(auto j:adj[temp])
-        {
-            if(vis[j]==0)
-            {
-                vis[j] = 1;
-                parent[j] = temp;
-                q.push(j);
-            }
-        }
-    }
-}
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define ll long long
 
 void solve()
 {
-    ll n,m;
-    cin >> n >> m;
-    vector<vector<ll>> adj(n+1);
-    vector<ll> parent(n+1);
-    vector<ll> vis(n+1,0);
-    for(int i = 0;i<=n;++i)
-    {
-        parent[i] = -1;
-    }
-    for(int i = 0;i<m;i++)
-    {
-        int u,v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    bfs(adj,parent,vis);
-    if(parent[n]==-1)
-    {
-        cout << "IMPOSSIBLE\n";
-        return;
-    }
-    int candidate = n;
-    stack<ll> st;
-    while(candidate!=-1)
-    {
-        st.push(candidate);
-        candidate = parent[candidate];
-    }
-    cout << st.size() <<"\n";
-    while(!st.empty())
-    {
-        cout << st.top() <<" ";
-        st.pop();
-    }
-    cout <<"\n";
-    return;
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    cout << 1 <<" " << 1 <<" "<< a <<" "<< b <<"\n";
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     ll t;
-    //cin >> t;
-    t = 1;
+    cin >> t;
+    //t = 1;
     while(t--)
     {
         solve();
--- hardwork/test.py
+++ hardwork/test.py
@@ -1,52 +1,12 @@
-import math
-sieve = []
-pre = []
-cnt = 0
-def sieve_of_erost(n):
-    global sieve
-    sieve=[1]*(n+1) 
-    pre = [1]*(n+1)
-    i = 1
-    sieve[0]=0
-    for i in range(2,math.ceil(math.sqrt(n))):
-        if sieve[i]==1:
-            for j in range(i*i,n+1,i):
-                if j%i==0:
-                    sieve[j] = 0
-    global cnt
-    for i in range(2,n+1):
-        if(sieve[i]==0):
-            pre[cnt] = i 
-            cnt+=1 
-    #print(sieve)
-
-def solve():
-    n = int(input())
-    li = list(map(int,input().split()))
-    i = 0
-    j = 0
-    d = {}
-    ans = -1
-    while i<n and j<n:
-        if li[j] not in d:
-            d[li[j]] = 1 
-        else:
-            d[li[j]] +=1
-        if d[li[j]]==1 and sieve[li[j]]==1:
-            j+=1 
-        else:
-            ans = max(ans,j-i)
-            i=j
-            j=i+1
-    ans = max(ans,j-i)
-    # for i in range(n-1):
-    #     if li[i]%li[i+1]!=0 or li[i+1]%li[i]!=0:
-    #         ans = 2
-    #         print("HERE")
-    if ans == 1:
-        ans = -1
-    print(ans)
 t = int(input())
-sieve_of_erost(pow(10,6)+1)
 for _ in range(t):
-    solve()
+	n = int(input())
+	if n%2 == 1:
+		print("Bob")
+	else:
+		while n%4 == 0:
+			n //= 4
+		if n == 2:
+			print("Bob")
+		else:
+			print("Alice")
